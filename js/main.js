b299de40f9ed7ea484ae800e922214aee856ef7d
Ilia V <ilia.isachamp@gmail.com>
2020-08-07 20:46:47 +0300

    fix: google map fix

--- js/main.js
+++ js/main.js
@@ -101,3 +101,23 @@ $(document).ready(function () {
  
 
 });
+
+// находим все фреймы в документе
+let iframes = document.querySelectorAll("iframe")
+
+// перебираем массив (на самом деле здесь мы имеем дело с объектом Nodelist, но это неважно)
+for (let i = 0; i < iframes.length; i++){
+    let iframe = iframes[i],
+    
+    // сохраняем оригинальное значение srс
+    originalSrc = iframe.src
+    
+    // заменяем содержимое фрейма картинкой
+    iframe.src = "../images/cover.jpg"
+    
+    // при наведении курсора отображаем содержимое фрейма
+    iframe.addEventListener("mouseover", () => iframe.src = originalSrc)
+    
+    // возвращаем "кавер"
+    iframe.addEventListener("mouseout", () => iframe.src = "../images/cover.jpg")
+}
